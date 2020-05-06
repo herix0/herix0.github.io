c03947ae725a79b1914d9d0aaa2d9b902df8ed0b
herix0 <64878850+herix0@users.noreply.github.com>
2020-05-06 06:20:22 +0200

    Vvv

--- iredtv/4356dsmgfrsl56ads541hjk604gr4jop89/b7ff1e87aa658e07ff88722721965535f83c2c60sefouir4489.php
+++ iredtv/4356dsmgfrsl56ads541hjk604gr4jop89/b7ff1e87aa658e07ff88722721965535f83c2c60sefouir4489.php
@@ -1,35 +1,14 @@
 {"X":[{"A":"0","B":"BBsSOHheKichAQEtAwM5WBQyKysoWn0zCi0yHTsMPwJgDBYFC1F3ewsQPDUaOQMAGnITJSsyDlhVEhd0Gw8fKSVnRi84OhoWfWwUKScWER8TWwNEAFMaLgVmZjkJdjYjDScRJlUROwEUUGNjCRIKDikBBAoaAxcJBhAvUnhrMjIcHBd7CmR3Ki0MGhRrTmN3FCwCBgYqHHQQCxJoAl9iNiMXAHwmISw/fjAoGxEIRl0LLA1OAgQiKwdaDzIrHRVzYDEDCSwOKRk6BXgqKAkVCH5dECogMBkYBQUhRwEiWzACeXUuJnYuMgsnBTxrClQIOiYIBQADMw4SXQwcNHY1UQY0FkdiHB8OIXlWPwpnRi84UB5RUgUtMyU/DQYQMR9EOiZXNjECbWMIPTE4JiE4P343Mxs8NmcNFg03ERo8A18fWxQILhsrcn0wKi0pIDkHO2ZnDTM3LwZiBBwKFS0zIhs/HHsQCBUxMEh9bAwpEHsLAihgaRpYARRTd0M5BnUCETcAWzdmFw0uNS9CVmkTLyAIViAwAQQqADoKU2l3YxASEycFKFtjSwEiWzI4aWYgHQdBIjUdEThr","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/tumu.png","E":"0"}
 
-{"A":"14","B":"TRT EBA TV EĞİTİM","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"14"},
-
-{"A":"1","B":"ULUSAL KANALLAR","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"1"},
-
-{"A":"2","B":"BELGESEL KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"2"},
-
-{"A":"3","B":"SPOR KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"3"},
-
-{"A":"4","B":"HABER KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"4"},
-
-{"A":"5","B":"MÜZİK KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"5"},
-
-{"A":"6","B":"ÇOCUK KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"6"},
-
-{"A":"7","B":"SİNEMA KANALLARI","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"7"},
-
-{"A":"10","B":"YEREL KANALLAR","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"10"},
-
-{"A":"12","B":"DİNİ KANALLAR","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"12"},
-
-{"A":"8","B":"KURDİSH","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/haber.png","E":"8"},
-
-{"A":"11","B":"GERMANY","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"11"},
-
-{"A":"13","B":"AZERBAİJAN","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/ulusal.png","E":"13"},
-
-{"A":"9","B":"BELÇİKA/FRANSA","C":"","D":"http:\/\/www.mobilcanlitvizle.com\/kategorilogo\/diger.png","E":"9"}],
 
 
-"Y":[{"A":"0","B":"İletişim & Hata Bildir","C":"1","D":"ULUSAL KANALLAR","E":"İletişim ve hata bildirmek için bizimle irtibat kurun.","F":"https://4.bp.blogspot.com/-0yHz4iZlBBs/XWKnF3sJ26I/AAAAAAAAAOI/MSicRpaMiqgwIvpylUT3BSmwr9mQ9rf8ACPcBGAYYCw/s1600/kjH7gkh.png","G":"https://iletisim93.blogspot.com/?m=1","H":"webplayer","I":"false","J":"direct","K":"","L":"","M":"","R":"","N":"","O":"03.05.2019 16:32:35","P":0},
+],"Y":[
+
+
+
+
+
+{"A":"0","B":"İletişim & Hata Bildir","C":"1","D":"ULUSAL KANALLAR","E":"İletişim ve hata bildirmek için bizimle irtibat kurun.","F":"https://4.bp.blogspot.com/-0yHz4iZlBBs/XWKnF3sJ26I/AAAAAAAAAOI/MSicRpaMiqgwIvpylUT3BSmwr9mQ9rf8ACPcBGAYYCw/s1600/kjH7gkh.png","G":"https://iletisim93.blogspot.com/?m=1","H":"webplayer","I":"false","J":"direct","K":"","L":"","M":"","R":"","N":"","O":"03.05.2019 16:32:35","P":0},
 
 {"A":"03","B":"Telegram Grubumuz","C":"1","D":"ULUSAL KANALLAR","E":"Yeniliklerden ilk siz haberdar olun.","F":"https://1.bp.blogspot.com/-7vi-miz6tSc/XWseU6ipwbI/AAAAAAAAATc/qqvWZ8xswQ0YzCW4VCC4cBarOgE4aSIhwCLcBGAs/s1600/PicsArt_09-01-03.20.32.png","G":"https://t.me/joinchat/MNLF0w-V4XwQZJOSr6kzMw","H":"openbrowser","I":"false","J":"direct","K":"","L":"","M":"","R":"","N":"","O":"03.05.2019 16:32:35","P":0},
 
