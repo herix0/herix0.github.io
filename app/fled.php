a6d4459a18127af7bff84cf5d245defaa08d4021
herix0 <64878850+herix0@users.noreply.github.com>
2020-10-21 21:28:31 +0200

    fff

--- app/fled.php
+++ app/fled.php
@@ -1,6 +1,6 @@
 {"MobilTurk":[
 {"ads_tip":"Admob",
-"main_link":"http://213.128.89.165/TEMEL/SowaProGuide/SowaProGuide_LinkV3.php",
+"main_link":"https://github.com/herix0/herix0.github.io/raw/master/app/pays.php",
 "app_id":"ca-app-pub-9133302355759736~1528781501",
 "banner_unit_id":"ca-app-pub-9133302355759736/7084182901",
 "inter_unit_id":"ca-app-pub-9133302355759736/1831856222",
