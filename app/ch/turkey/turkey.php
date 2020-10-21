592f971e481e9113ce4f84bd704e65cc7be39ba0
herix0 <64878850+herix0@users.noreply.github.com>
2020-10-21 22:34:11 +0200

    cc

--- app/ch/turkey/turkey.php
+++ app/ch/turkey/turkey.php
@@ -21,7 +21,7 @@
 "kategory":"Tüm Kanallar",
 "ulke_flag":"http://213.128.89.165/Main/Flag/owl_bayrak/turkey.png",
 "ulke":"Turkey\/Tam Liste",
-"cat_url":"http://213.128.89.165/TEMEL/Primavera/turkey/channel_turkey.txt",
+"cat_url":"https://dl.dropboxusercontent.com/s/zilrx8alabd2l48/tum.txt?dl=0",
 "cat_foto":"http://www.freelogovectors.net/wp-content/uploads/2013/02/TV-Icon-9.png"},
 {"cat_id":888,
 "kategory":"Ulusal",
