9308e20ee4cd77d3fb722d89bee8b5840a1fc408
herix0 <64878850+herix0@users.noreply.github.com>
2020-10-25 14:24:08 +0100

    cff

--- app/fled.php
+++ app/fled.php
@@ -1,8 +1,8 @@
 {"MobilTurk":[
 {"ads_tip":"Admob",
 "main_link":"https://github.com/herix0/herix0.github.io/raw/master/app/pays.php",
-"app_id":"ca-app-pub-9133302355759736~1528781501",
-"banner_unit_id":"ca-app-pub-9133302355759736/7084182901",
-"inter_unit_id":"ca-app-pub-9133302355759736/1831856222",
+"app_id":"",
+"banner_unit_id":"",
+"inter_unit_id":"",
 "iron_id":""},
 ]}
