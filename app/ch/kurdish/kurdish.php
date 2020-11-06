97a6cca0eccde8d8edcbc5906622276ae908531f
herix0 <64878850+herix0@users.noreply.github.com>
2020-11-06 01:19:34 +0100

    cc

--- app/ch/kurdish/kurdish.php
+++ app/ch/kurdish/kurdish.php
@@ -10,6 +10,6 @@
 "kategory":"Channels",
 "ulke_flag":"http://213.128.89.165/Main/Flag/owl_bayrak/spain.png",
 "ulke":"Kurdish",
-"cat_url":"https://github.com/herix0/herix0.github.io/raw/master/app/ch/kurdish/channel_kurdish.txt",
+"cat_url":"https://dl.dropboxusercontent.com/s/g28o57m2z21segu/channel_kurdish.txt?dl=0",
 "cat_foto":"http://www.freelogovectors.net/wp-content/uploads/2013/02/TV-Icon-9.png"},
 ]}
