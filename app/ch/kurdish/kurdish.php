a384fbdbba3b85f444fa14241c2bb02254521cc6
herix0 <64878850+herix0@users.noreply.github.com>
2020-11-06 01:07:44 +0100

    cc

--- app/ch/kurdish/kurdish.php
+++ app/ch/kurdish/kurdish.php
@@ -1,1 +1,15 @@
+{"Kategory":[
+{"cat_id":9998,
+"kategory":"Telegram Grubumuz",
+"ulke_flag":"https://1.bp.blogspot.com/-yuIcE3kYeOA/X6SL2N_v4bI/AAAAAAAABsk/GGBXystwMjcK_q2sKtWbYX4ySmim11AagCLcBGAsYHQ/s0/flag_kurdistan.png",
+"ulke":"Telegram Grubumuz",
+"cat_url":"https://t.me/joinchat/MNLF0w-V4XwQZJOSr6kzMw",
+"cat_foto":"https://1.bp.blogspot.com/-7vi-miz6tSc/XWseU6ipwbI/AAAAAAAAATc/qqvWZ8xswQ0YzCW4VCC4cBarOgE4aSIhwCLcBGAs/s1600/PicsArt_09-01-03.20.32.png"},
 
+{"cat_id":88888,
+"kategory":"Channels",
+"ulke_flag":"http://213.128.89.165/Main/Flag/owl_bayrak/spain.png",
+"ulke":"Kurdish",
+"cat_url":"https://github.com/herix0/herix0.github.io/raw/master/app/ch/kurdish/channel_kurdish.txt",
+"cat_foto":"http://www.freelogovectors.net/wp-content/uploads/2013/02/TV-Icon-9.png"},
+]}
