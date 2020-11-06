e4be95d5357da9db13a94a6fc2d5426d84bebe17
herix0 <64878850+herix0@users.noreply.github.com>
2020-11-07 00:58:26 +0100

    xc

--- app/ch/azerbaijan/azerbaijan.php
+++ app/ch/azerbaijan/azerbaijan.php
@@ -1,1 +1,22 @@
+{"Kategory":[
+{"cat_id":9999,
+"kategory":"Telegram Grubumuz",
+"ulke_flag":"http://213.128.89.165/Main/Flag/owl_bayrak/azeri.png",
+"ulke":"Telegram Grubumuz",
+"cat_url":"https://t.me/joinchat/MNLF0w-V4XwQZJOSr6kzMw",
+"cat_foto":"https://1.bp.blogspot.com/-7vi-miz6tSc/XWseU6ipwbI/AAAAAAAAATc/qqvWZ8xswQ0YzCW4VCC4cBarOgE4aSIhwCLcBGAs/s1600/PicsArt_09-01-03.20.32.png"},
 
+{"cat_id":9997,
+"kategory":"Yayın da Ne Var?",
+"ulke_flag":"http://213.128.89.165/Main/Flag/owl_bayrak/azeri.png",
+"ulke":"",
+"cat_url":"",
+"cat_foto":"https://www.timeinc.com/wp-content/uploads/2014/03/whatsontv_logo.png"},
+
+{"cat_id":888,
+"kategory":"TV Kanalları",
+"ulke_flag":"http://213.128.89.165/Main/Flag/owl_bayrak/azeri.png",
+"ulke":"Azerbaycan\/Tam Liste",
+"cat_url":"https://dl.dropboxusercontent.com/s/6u0z6tbn41k3o13/channel_azerbaijan.txt?dl=0",
+"cat_foto":"http://www.freelogovectors.net/wp-content/uploads/2013/02/TV-Icon-9.png"},
+]}
