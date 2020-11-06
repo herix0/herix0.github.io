485c97547f5a4ebc8d2033a39544695d1ba5cff6
herix0 <64878850+herix0@users.noreply.github.com>
2020-11-06 01:51:26 +0100

    cc

--- app/ch/kurdish/kurdish.php
+++ app/ch/kurdish/kurdish.php
@@ -6,7 +6,7 @@
 "cat_url":"https://t.me/joinchat/MNLF0w-V4XwQZJOSr6kzMw",
 "cat_foto":"https://1.bp.blogspot.com/-7vi-miz6tSc/XWseU6ipwbI/AAAAAAAAATc/qqvWZ8xswQ0YzCW4VCC4cBarOgE4aSIhwCLcBGAs/s1600/PicsArt_09-01-03.20.32.png"},
 
-{"cat_id":88888,
+{"cat_id":888,
 "kategory":"Channels",
 "ulke_flag":"https://1.bp.blogspot.com/-yuIcE3kYeOA/X6SL2N_v4bI/AAAAAAAABsk/GGBXystwMjcK_q2sKtWbYX4ySmim11AagCLcBGAsYHQ/s0/flag_kurdistan.png",
 "ulke":"Kurdish",
