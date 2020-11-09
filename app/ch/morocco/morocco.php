0e3a429f648c5240ee86331652c359157f084e99
herix0 <64878850+herix0@users.noreply.github.com>
2020-11-09 03:29:53 +0100

    ccc

--- app/ch/morocco/morocco.php
+++ app/ch/morocco/morocco.php
@@ -10,6 +10,6 @@
 "kategory":"Channels",
 "ulke_flag":"http://213.128.89.165/Main/Flag/owl_bayrak/morocco.png",
 "ulke":"Morocco",
-"cat_url":"https://github.com/herix0/herix0.github.io/raw/master/app/ch/marocco/channel_marocco.txt",
+"cat_url":"https://github.com/herix0/herix0.github.io/raw/master/app/ch/morocco/channel_morocco.txt",
 "cat_foto":"http://www.freelogovectors.net/wp-content/uploads/2013/02/TV-Icon-9.png"},
 ]}
