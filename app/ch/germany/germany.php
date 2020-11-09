a0ca653455d2052f805b287ceb02e02a1631ca06
herix0 <64878850+herix0@users.noreply.github.com>
2020-11-09 02:59:52 +0100

    ccc

--- app/ch/germany/germany.php
+++ app/ch/germany/germany.php
@@ -1,1 +1,16 @@
 
+{"Kategory":[
+{"cat_id":9999,
+"kategory":"Telegram Grubumuz",
+"ulke_flag":"https://1.bp.blogspot.com/-I4ms6mqyBI4/X5GHSfPOHpI/AAAAAAAABrM/DMSNOXs15FU8eLg2sKuGECBsSDmfqHgpgCLcBGAsYHQ/s0/turkey.png",
+"ulke":"Telegram Grubumuz",
+"cat_url":"https://t.me/joinchat/MNLF0w-V4XwQZJOSr6kzMw",
+"cat_foto":"https://1.bp.blogspot.com/-7vi-miz6tSc/XWseU6ipwbI/AAAAAAAAATc/qqvWZ8xswQ0YzCW4VCC4cBarOgE4aSIhwCLcBGAs/s1600/PicsArt_09-01-03.20.32.png"},
+
+{"cat_id":888,
+"kategory":"Channels",
+"ulke_flag":"http://213.128.89.165/Main/Flag/owl_bayrak/germany.png",
+"ulke":"Germany",
+"cat_url":"https://github.com/herix0/herix0.github.io/raw/master/app/ch/germany/channel_germany.txt",
+"cat_foto":"http://www.freelogovectors.net/wp-content/uploads/2013/02/TV-Icon-9.png"},
+]}
