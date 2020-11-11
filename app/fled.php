c44b26de970df2d1a52a8961980fd2e594a6561d
herix0 <64878850+herix0@users.noreply.github.com>
2020-11-11 16:07:08 +0100

    Create fled.php

---
+++ app/fled.php
@@ -0,0 +1,8 @@
+{"Mobilim":[
+{"ads_tip":"Admob",
+"main_link":"https://github.com/herix0/herix0.github.io/raw/master/app/pays.php",
+"app_id":"ca-app-pub-9133302355759736~1528781501",
+"banner_unit_id":"ca-app-pub-9133302355759736/7084182901",
+"inter_unit_id":"ca-app-pub-9133302355759736/1831856222",
+"iron_id":""},
+]}
