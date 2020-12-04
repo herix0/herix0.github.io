f9da34d1239e59db65150809a3312e84a42d9bcb
herix0 <64878850+herix0@users.noreply.github.com>
2020-12-05 00:03:19 +0100

    xd

--- app/pays.php
+++ app/pays.php
@@ -1,8 +1,8 @@
 {"Ulke":[
 {"ulke_id":9999,
-"country":"🛡Eski v4.8.4 Ücretli Full Özellikli Yükle(1 Gün ⬇️%30 İndirim)",
+"country":"🛡Eski v4.8.4 Ücretli Full Özellikli Yükle",
 "ulke_flag":"https://1.bp.blogspot.com/-I4ms6mqyBI4/X5GHSfPOHpI/AAAAAAAABrM/DMSNOXs15FU8eLg2sKuGECBsSDmfqHgpgCLcBGAsYHQ/s0/turkey.png",
-"ulke":"🛡Eski v4.8.4 Ücretli Full Özellikli Yükle(5 Gün ⬇️%30 İndirim)",
+"ulke":"🛡Eski v4.8.4 Ücretli Full Özellikli Yükle",
 "ulke_url":"https://play.google.com/store/apps/details?id=com.canliglobaltvpro.android",
 "country_foto":"https://1.bp.blogspot.com/-qSqhbbYFzbM/X7_VDL4t1eI/AAAAAAAABtQ/55FxOHeaUEAVejIGRqHH9RmargAXeiKmgCLcBGAsYHQ/s0/PicsArt_11-26-05.14.01.png"},
 
